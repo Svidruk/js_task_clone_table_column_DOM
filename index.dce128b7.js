160be4a74833d0a8aeea579a9c3de4abe1c31a65
Svidruk <mr.svid999@gmail.com>
2024-09-02 22:40:42 +0300

    make build

---
+++ index.dce128b7.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table"),l=0;l<e.rows.length;l++){var r=e.rows[l],o=r.cells[1].cloneNode(!0);r.insertBefore(o,r.cells[r.cells.length-1])}
+//# sourceMappingURL=index.dce128b7.js.map
